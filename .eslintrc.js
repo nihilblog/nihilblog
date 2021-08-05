27eb7e31b7747b9f782aac13b717c53a4b22d0af
NIHILncunia <nihil_ncunia@naver.com>
2021-08-05 20:59:36 +0900

    2021 0805 eslint 재설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,66 +9,77 @@ module.exports = {
   extends: [
     'airbnb',
     'airbnb/hooks',
-    'prettier',
     'plugin:import/errors',
     'plugin:import/warnings',
     'eslint:recommended',
-    'plugin:prettier/recommended',
+    'plugin:react/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2021,
     sourceType: 'module',
   },
-  plugins: [ 'import', 'react', 'react-hooks', 'prettier' ],
+  plugins: [ 'import', 'react', 'react-hooks', ],
   rules: {
-    'prettier/prettier': 'off',
     'no-console': 'off',
     'no-unused-vars': 'warn',
     'no-unexpected-multiline': 'off',
     'no-use-before-define': 'off',
     'spaced-comment': 'off',
     'no-param-reassign': 'off',
-    'eol-last': [ 'warn', 'always' ],
+    'eol-last': [ 'warn', 'always', ],
     'no-plusplus': 'off',
     'no-restricted-syntax': 'off',
     'array-callback-return': 'off',
     'consistent-return': 'off',
     'no-nested-ternary': 'off',
-    'quotes': [ 'warn', 'single' ],
-    'semi': [ 2, 'always', ],
-    'array-bracket-spacing': [ 1, 'always', {
-      arraysInArrays: true,
-      singleValue: true,
-      objectsInArrays: true,
-    }, ],
-    'object-curly-spacing': [ 1, 'always', ],
+    quotes: [ 'warn', 'single', { allowTemplateLiterals: true, }, ],
+    semi: [ 'error', 'always', ],
+    'array-bracket-spacing': [
+      'warn',
+      'always',
+      {
+        arraysInArrays: true,
+        singleValue: true,
+        objectsInArrays: true,
+      },
+    ],
+    'object-curly-spacing': [ 'warn', 'always', ],
     'no-shadow': 'off',
-  
+    indent: [ 'warn', 2, ],
+    'comma-dangle': [ 'warn', {
+      arrays: 'always',
+      functions: 'never',
+      objects: 'always',
+      imports: 'never',
+      exports: 'never',
+    }, ],
+    'jsx-quotes': [ 'error', 'prefer-single', ],
+
     'import/extensions': 'off',
     'import/no-extraneous-dependencies': 'off',
     'import/no-unresolved': 'off',
     'import/prefer-default-export': 'off',
-    
-    'jsx-quotes': [ 'error', 'prefer-single' ],
+
     'react/jsx-props-no-spreading': 'off',
     'react/forbid-prop-types': 'off',
-    'react/jsx-filename-extension': [
+    'react/jsx-filename-extension': [ 'error', { extensions: [ '.js', '.jsx', ], }, ],
+    'react/no-danger': 'off',
+    'react/jsx-curly-brace-presence': [
+      'warn',
+      { props: 'never', children: 'never', },
+    ],
+    'react/require-default-props': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+
+    'jsx-a11y/anchor-is-valid': [
       'error',
       {
-        extensions: [ '.js', '.jsx' ],
+        components: [ 'Link', ],
+        specialLink: [ 'hrefLeft', 'hrefRight', ],
+        aspects: [ 'invalidHref', 'preferButton', ],
       },
     ],
-    'react/no-danger': 'off',
-    'react/jsx-curly-brace-presence': [ 'warn', { props: 'never', children: 'never' } ],
-    'react/require-default-props': 'off',
-    
-    'react-hooks/exhaustive-deps': 'off',
-  
-    'jsx-a11y/anchor-is-valid': [ 'error', {
-      'components': [ 'Link' ],
-      'specialLink': [ 'hrefLeft', 'hrefRight' ],
-      'aspects': [ 'invalidHref', 'preferButton' ]
-    } ],
     'jsx-a11y/anchor-has-content': 'off',
     'jsx-a11y/no-static-element-interactions': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
